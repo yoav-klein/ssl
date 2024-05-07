57cbe9e5f37678b5f78ca8d8785f644fd9218783
yoklein <yoav.klein@cyberark.com>
2024-05-07 06:47:49 +0000

    Use config file:

--- using_config_file/script.sh
+++ using_config_file/script.sh
@@ -6,10 +6,14 @@ cert=server.crt
 
 # create private key and CSR
 openssl genrsa -out $private_key 2048
-openssl req -new -key $private_key -subj="/CN=*.yoav.net" -out $csr -config ssl.conf -reqexts v3_req
+openssl req -new -key $private_key -subj="/CN=*.yoav.net" -out $csr -config req.conf -reqexts v3_req
 
 # another alternative - using -addext option
 #openssl req -new -sha384  -addext subjectAltName=DNS.1:*.yoav-klein.com,DNS.2:yoav-klein.com -subj="/CN=yoav-klein.com" -key $private_key -out $csr
+
+echo "We have created a CSR with extensions in it"
+echo "Note that in order for the extensions to be in the certificate,"
+echo "The CA must include them in the signing process."
 
 # gen CA
 
@@ -17,8 +21,8 @@ openssl genrsa -out $ca.key 2048
 openssl req -new -subj "/CN=*.my.authority" -x509 -days 365 -key $ca.key -sha256 -out $ca.cert
 
 # sign request
+
 openssl x509 -req -days 365 -sha256 -in $csr -CA $ca.cert -CAkey $ca.key \
-    	-CAcreateserial -out $cert 
+    	-CAcreateserial -out $cert -extfile sign.conf 
         
 
-#-extfile $ext_file -extensions $extensions
