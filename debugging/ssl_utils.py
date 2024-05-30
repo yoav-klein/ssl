d68b76525c7e1af3d7328ab28beb9010b961129b
Yoav Klein <yoavklein25@gmail.com>
2024-05-30 16:15:22 +0000

    ssl_utils.py

--- debugging/ssl_utils.py
+++ debugging/ssl_utils.py
@@ -14,6 +14,12 @@ import os
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
+def read_bundle(cert_file):
+    with open(cert_file, 'rb') as f:
+        cert_data = f.read()
+    
+    cert = x509.load_pem_x509_certificates(cert_data)
+    return cert
 
 def read_certificate(cert_file):
     with open(cert_file, 'rb') as f:
@@ -83,6 +89,21 @@ def find_cert_by_cn(cn: str, path: str):
     print(f"Found: {count}")
 
 
+def find_cert_in_bundle(cn: str, path: str):
+    count = 0
+    cert_list = read_bundle(path)
+    for cert_data in cert_list:
+        cert = extract_certificate_data(cert_data)
+        subject = cert['subject']
+        for rdn in subject:
+            if rdn.rfc4514_attribute_name == "CN":
+                if rdn.value == cn:
+                    count += 1
+                    display_cert(cert)
+
+    print(f"Found: {count}")        
+
+
 def display_list(path: str):
     cert_list = list_certificates_in_dir(path)
     for cert in cert_list:
@@ -92,7 +113,7 @@ def display_list(path: str):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="SSL Diagnostics Tool")
-    subparsers = parser.add_subparsers(dest="command", help="Choose a command")
+    subparsers = parser.add_subparsers(dest="command", required=True, help="Choose a command")
 
     list_parser = subparsers.add_parser("list-certs", help="List certificates in directory")
     list_parser.add_argument("--path", type=str, help="Path to directory", required=True)
@@ -100,6 +121,12 @@ def parse_arguments():
     find_cert_parser = subparsers.add_parser("find-cert", help="Find certificate in directory")
     find_cert_parser.add_argument("--cn", type=str, help="CN to find", required=True)
     find_cert_parser.add_argument("--path", type=str, help="Path to directory", required=True)
+
+    find_cert_parser1 = subparsers.add_parser("find-cert-in-bundle", help="Find certificate in directory")
+    find_cert_parser1.add_argument("--cn", type=str, help="CN to find", required=True)
+    find_cert_parser1.add_argument("--path", type=str, help="Path to bundle", required=True)
+
+
 
     return parser.parse_args()
 
@@ -111,6 +138,9 @@ def main():
 
     if args.command == "find-cert":
         find_cert_by_cn(args.cn, args.path)
+
+    if args.command == "find-cert-in-bundle":
+        find_cert_in_bundle(args.cn, args.path)
 
 
 if __name__ == "__main__":
